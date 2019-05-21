52010a20f69b7fd0d3b558ed97f5f225052c3d4f
Tasha Coote <tasha0coote@gmail.com>
2019-05-21 00:38:37 -0700

    Move 'Handle User Action' codeblock into a new function main_menu(). This code will need to be called after the completion of each user action chain. Add call to main_menu() at the end of new_entry() and display_prev_entry(). Add call to main_menu() after an invalid entry in main_menu().

--- testingPythonInIdle1.0.py
+++ testingPythonInIdle1.0.py
@@ -15,21 +15,29 @@ def new_entry():
     drug_name = input("Drug name:")
     num_pills = input("Total number of pills:")
     pills_per_dose = input("Number of pills taken per dose:")
+    main_menu()
 
 
 def display_prev_entry():
     print(drug_name)
+    print(num_pills)
+    print(pills_per_dose)
+    main_menu()
 
+
+def main_menu():
+    print("Press 'd' if you would like to display the previous entry.")
+    go_to = input("Press 'n' if you would like to submit a new entry.")
+    if go_to == "n":
+        print("n pressed")
+        new_entry()
+    elif go_to == "d":
+        print("d pressed")
+        display_prev_entry()
+    else:
+        print("Invalid entry.")
+        main_menu()
 
 
 # HANDLE USER ACTION
-print("Press 'd' if you would like to display the previous entry.")
-go_to = input("Press 'n' if you would like to submit a new entry.")
-if go_to == "n":
-    print("n pressed")
-    new_entry()
-elif go_to == "d":
-    print("d pressed")
-    display_prev_entry()
-else:
-    print("Invalid entry.")
+main_menu()
