b83a73052fb46d8b90b7cf34b8ef31a80194956f
Tasha Coote <tasha0coote@gmail.com>
2019-05-21 17:25:55 -0700

    Change all references using any variant of num_pills into a variant of total_pills.:

--- testingPythonInIdle1.0.py
+++ testingPythonInIdle1.0.py
@@ -10,16 +10,16 @@ print("Celebrate! You are demonstrating your knowledge! And writing code!")
 #to lock each variable into the type we want.
 #Will need to implement error handling later.
 drug_name = "empty"
-num_pills = 0
+total_pills = 0
 pills_per_dose = 0
 
 # OBJECTS
 # Initializing with "c_drug_name" etc. instead of "drug_name", while I confirm
 # variable scope.
 class Prescription:
-    def __init__(self, c_drug_name, c_num_pills, c_pills_per_dose):
+    def __init__(self, c_drug_name, c_total_pills, c_pills_per_dose):
         self.c_drug_name = c_drug_name
-        self.c_num_pills = c_num_pills
+        self.c_total_pills = c_total_pills
         self.c_pills_per_dose = c_pills_per_dose
 
 
@@ -61,14 +61,14 @@ def create_prescription(conn, prescription):
 # FUNCTIONS
 def new_entry():
     drug_name = input("Drug name:")
-    num_pills = input("Total number of pills:")
+    total_pills = input("Total number of pills:")
     pills_per_dose = input("Number of pills taken per dose:")
     main_menu()
 
 
 def display_prev_entry():
     print(drug_name)
-    print(num_pills)
+    print(total_pills)
     print(pills_per_dose)
     main_menu()
 
@@ -76,14 +76,14 @@ def display_prev_entry():
 def test_prescription():
     # get 3 user inputs
     l_drug_name = input("Drug name:")
-    l_num_pills = input("Total number of pills:")
+    l_total_pills = input("Total number of pills:")
     l_pills_per_dose = input("Number of pills taken per dose:")
 
-    my_prescription = Prescription(l_drug_name, l_num_pills, l_pills_per_dose)
+    my_prescription = Prescription(l_drug_name, l_total_pills, l_pills_per_dose)
     # print values from object
     print("\nThis is what we have on file for you:")
     print("Drug name: " + my_prescription.c_drug_name)
-    print("Number of total pills: " + my_prescription.c_num_pills)
+    print("Number of total pills: " + my_prescription.c_total_pills)
     print("Number of pills per dose: " + my_prescription.c_pills_per_dose)
     main_menu()
 
@@ -124,7 +124,7 @@ if conn is not None:
 elif conn is None:
     print("Conn is None.")
 else:
-    print("Error! Cannot create the database connection.")s
+    print("Error! Cannot create the database connection.")
 
 with conn:
      l_prescription = ('Motrin', 47, 3)
